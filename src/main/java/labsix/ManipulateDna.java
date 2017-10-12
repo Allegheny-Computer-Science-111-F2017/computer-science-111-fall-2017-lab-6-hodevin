00ebe5f0a5af2f3d4940cae030403ac16f397eac
Devin Ho <hod@aldenv156.allegheny.edu>
2017-10-12 15:57:14 -0400

    Lab 6 Done

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,10 +3,20 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
-public class ManipulateDna {
+/** This class manipulates the DNA string for bioinformatics.
+ *  author @Devin Ho
+**/
 
+public class ManipulateDna {
+  /** The main entry point for ManipulateDna.
+   *  The source code inside of this method is executed when the program runs in the command line. 
+   *  The output of this program will appear in the terminal window.
+   *  The printline statements will display the outputs.
+  **/
+ 
   public static void main(String[] args) {
     // display the name of the programmer and the date
     System.out.println("Gregory M. Kapfhammer " + new Date());
@@ -21,17 +31,43 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string " 
+        + "\"" + dna + "\"" + ".");
+    System.out.println();
+    System.out.println("Enter a string containing only C, G, T, and A: " + dna.toUpperCase());
     // Step Two: Compute the complement of the DNA String
+    String dnaUp = dna.toUpperCase();
+    String p1 = dnaUp.replace('A', 't');
+    String p2 = p1.replace('T', 'a');
+    String p3 = p2.replace('C', 'g');
+    String p4 = p3.replace('G', 'c');
+    System.out.println("The complement of ACTG is " + p4.toUpperCase());
 
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
+    Random generator = new Random();
+    int num1 = generator.nextInt(4);
+    char insertion = "ACTG".charAt(num1);
+
+    String result1 = insertion + p4.toUpperCase();
+    System.out.println("Inserting " + insertion + " at position 0 gives " + result1);
 
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    int num2 = generator.nextInt(dna.length());
+    String insertion2 = dna.substring(0, num2);
+    String deletion = dna.substring(num2 + 1);
+    System.out.println("Deleting from position " + num2 + " gives " 
+        + (insertion2 + deletion).toUpperCase());
+    
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int num3 = generator.nextInt(4);
+    int num4 = generator.nextInt(4);
+    char addletter = "ACTG".charAt(num3);
+    String s1 = dna.substring(0, num4) + addletter + dna.substring(num4 + 1);
+    System.out.println("Changing position " + num4 + " gives " + (s1.toUpperCase()));
     // Step Six: Display a final thankyou message
-
+    System.out.println();
+    System.out.println("Thanks for using the ManipulateDna program.\nHave an awesome day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,10 @@
-# Reflection by
+# Reflection by Devin Ho
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. I read in the file
+and printed out the input from the user. Then, I used the "substring" and
+"charAt" methods. I also used the Random package from 'java.util' to generator
+random numbers. "nextInt" is the method that I used for that.
+
+The most challenging step of this lab was figuring out how to incorporate the
+random number generator in order to make insertions, deletions, and
+replacements in each DNA string.
